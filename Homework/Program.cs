1230daf69bf4f66605609661777b0dc0956cab92
Andrey <a_mikhaylov@inbox.ru>
2022-09-03 18:20:31 +0300

    Seminar5

---
+++ Homework/Program.cs
@@ -0,0 +1,190 @@
+﻿// //==== Задача 37:
+// // Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент,
+// // второй и предпоследний и т.д. Результат запишите в новом массиве.
+// // [1 2 3 4 5] -> 5 8 3
+// // [6 7 3 6] -> 36 21
+
+// //метод создает массив из случайных чисел
+// int[] massRandom(int length)
+// {
+//     int[] MyArr = new int[length];
+
+//     for (int i = 0; i < length; i++)
+//     {
+//         MyArr[i] = new Random().Next(-9, 10);
+//     }
+//     Console.WriteLine(String.Join(" ", MyArr));
+//     return MyArr;
+// }
+
+// Console.Write("Задайте длину массива: ");
+// int count = Convert.ToInt32(Console.ReadLine());
+
+// // Определям массив четный или нет и создаем новый / 2;
+
+// int nouch = 0; //четный массив признак
+// int Length = count / 2;
+// if (count % 2 != 0)
+// {
+//     nouch = 1; //признак нечетный массив
+//     Length += 1;
+// }
+
+// int[] MyArr = massRandom(count);
+// int[] NewArr = new int[Length];
+
+// int j = 0;
+// for (; j < count -1; j++)
+// {
+//     NewArr[j] = MyArr[j] * MyArr[count - 1];
+//     count -= 1;
+// }
+
+// if (nouch == 1)
+// {
+//     // добавляем нечетный элемент старого массива в новый массив
+//     NewArr[j] = MyArr[j];
+// }
+// //else { }
+
+// Console.WriteLine($"[{(String.Join(", ", NewArr))}]");
+
+
+
+// //==== Задача 34:
+// // Задайте массив заполненный случайными положительными трёхзначными числами.
+// // Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// // [345, 897, 568, 234] -> 2
+
+
+// // метод заполнения массива
+// int[] massRandom(int length)
+// {
+//     int[] MyArr = new int[length];
+
+//     for (int i = 0; i < length; i++)
+//     {
+//         MyArr[i] = new Random().Next(100, 1000);
+//     }
+//     Console.WriteLine(String.Join(" ", MyArr));
+//     return MyArr;
+// }
+
+// // метод подсчета четных с выводом
+// void evenNum(int[] mas)
+// {
+// int len = mas.Length;
+// int count = 0;
+
+// for(int i =0; i < len; i++)
+// {
+// if(mas[i] % 2  == 0)
+// {
+//     count++;
+// }
+// }
+
+// Console.Write("количество четных чисел ="+count );
+// }
+
+// // сама программа
+// Console.Write("Задайте длину массива: ");
+// int index = Convert.ToInt32(Console.ReadLine());
+// int[] mass = massRandom(index);
+// evenNum(mass);
+
+
+
+
+//==== Задача 36:
+// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// метод создания произвольного массива
+// int[] massRandom()
+// {
+
+// Console.Write("Задайте длину массива: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите min число массива: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите max число массива: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+//     int[] array = new int[length];
+//     for(int i = 0; i < length; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+// // подсчет суммы элементов нечет позиция
+// void sumOdd(int[] Odd)
+// {
+//     int summ = 0;
+//     for( int i = 1; i < Odd.Length; i+=2)
+//     {
+//         summ += Odd[i];
+//     }
+//     Console.Write("Сумма не чет =: "+summ);
+
+// }
+
+// int[] ara = massRandom();
+// Console.WriteLine(String.Join(" ", ara));
+// sumOdd(ara);
+
+
+//====Задача 38:
+// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным
+// элементов массива. Например: [3 7 22 2 78] -> 76
+
+double[] massRandom()
+{
+    Console.Write("Задайте длину массива: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите min число массива: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите max число массива: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    double[] array = new double[length];
+    int[] res = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        res[i] = new Random().Next(min, max); //заполнение массива целым
+        array[i] = Convert.ToUInt64(res[i]);  //конвертирование в double
+    }
+
+    return array;
+}
+
+// поиск max и min/ разность
+
+void differ(double[] mas)
+{
+    Console.WriteLine($"[{(String.Join(" ", mas))}]");
+    double differ = 0;
+    int i = 0;
+    double Max = mas[i];
+    double Min = mas[i + 1];
+
+    for (i = 0; i < mas.Length; i++)
+    {
+        if (Max < mas[i]) Max = mas[i];
+        if (Min > mas[i]) Min = mas[i];
+    }
+    differ = Max - Min;
+    Console.Write($" Max - Min =: {differ}");
+}
+double[] Mass = massRandom();
+differ(Mass);
---
+++ Seminar005/Program.cs
@@ -0,0 +1,191 @@
+﻿//==== Задача 31:
+// Задайте массив из 12 элементов, заполненный случайными
+// числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных
+// элементов массива. Например, в массиве
+// [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+// int[] Array()
+
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// //Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+//  using System;
+
+//  class MyNewClass
+//  {
+
+//     public static  void MyTask1()
+//     {
+//         //         : Задайте массив из 12 элементов, заполненный
+//         // случайными числами из промежутка [-9, 9]. Найдите сумму
+//         // отрицательных и положительных элементов массива.
+//        int [] MyArr = new int[12];
+//        int SumMin =0, SumPl = 0;
+
+//        for (int i = 0; i< MyArr.Length; i++)
+//        {
+//             MyArr[i]= new Random().Next(-9,10);
+//             if (MyArr[i]<0)  SumMin += MyArr[i];
+//             else SumPl += MyArr[i];
+
+//        }
+
+//        Console.WriteLine(String.Join(", ", MyArr ));
+//        Console.WriteLine($"Сумма отрицательных {SumMin}");
+//        Console.WriteLine($"Сумма положительных {SumPl}");
+
+//     }
+
+//     public static  void MyTask32()
+//     {
+//         // Напишите программу замена элементов
+//         // массива: положительные элементы замените на
+//         // соответствующие отрицательные, и наоборот.
+
+//         int[] MyArr = new int[12];
+//         for (int i=0; i< MyArr.Length; i++)
+//         {
+//             MyArr[i] = new Random().Next(-9,10);
+//             Console.Write($" {MyArr[i]} ");
+//             MyArr[i] = -MyArr[i];
+//         }
+//         Console.WriteLine(String.Join(" ", MyArr));
+
+
+//     //    int [] MyArr = new int[12];
+//     //     // Задаем массив
+//     //    for (int i = 0; i< MyArr.Length; i++)
+//     //    {
+//     //         MyArr[i]= new Random().Next(-9,10);
+
+//     //    }
+//     //    Console.WriteLine(String.Join(", ", MyArr ));
+
+//     //     // Меняем элементы
+//     //     for (int i=0; i < MyArr.Length; i++ )
+//     //     {
+//     //         MyArr[i] = -MyArr[i];
+//     //     }
+//     //     Console.WriteLine(String.Join(", ", MyArr ));
+//     }
+
+//     public static  bool MyTask33( )
+//     {
+//         // Задайте массив. Напишите программу, которая
+//         // определяет, присутствует ли заданное число в массиве.
+
+//         int[] MyArr = new int[12];
+//         bool numInArr=false;
+//         for (int i=0; i< MyArr.Length; i++)
+//         {
+//             MyArr[i] = new Random().Next(-9,10);
+//         }
+//         Console.WriteLine(String.Join(" ", MyArr));
+
+//         Console.WriteLine("Введите число для поиска ");
+//         int num = Convert.ToInt32(Console.ReadLine());
+//              for (int i=0; i< MyArr.Length; i++)
+//         {
+//             if (MyArr[i] == num)  numInArr = true;
+//         }
+
+//         return numInArr;
+//     }
+//      public static int MyTask35()
+//      {
+//     // Задайте одномерный массив из 123 случайных чисел.
+//     // Найдите количество элементов массива, значения которых лежат в
+//     // отрезке [10,99].
+//     int[] MyArr = new int[123];
+//     int count=0;
+//     for (int i=0; i< MyArr.Length; i++)
+//     {
+//         MyArr[i] = new Random().Next(-900,1000);
+//         if  ((MyArr[i] >= 10) && (MyArr[i] <= 99))   count++;
+//     }
+
+//     Console.WriteLine(String.Join(" ", MyArr));
+//     System.Console.WriteLine(count);
+
+//     return count;
+//  }
+
+//    public static void MyTask37()
+//  {
+// Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве
+
+//метод создает массив из случайных чисел
+int[] massRandom(int length)
+{
+    int[] MyArr = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        MyArr[i] = new Random().Next(-9, 10);
+    }
+    Console.WriteLine(String.Join(" ", MyArr));
+    return MyArr;
+}
+
+Console.WriteLine("Задайте длину массива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+// Определям массив четный или нет и создаем новый / 2;
+
+int nouch = 0; //четный массив
+int Length = count / 2;
+if (count % 2 != 0)
+{
+    nouch = 1; //признак нечетный массив
+    Length += 1;
+}
+
+int[] MyArr = massRandom(count);
+int[] NewArr = new int[Length];
+
+int j = 0;
+for (; j < count - 1; j++)
+{
+    NewArr[j] = MyArr[j] * MyArr[count - 1];
+    count -= 1;
+}
+
+if (nouch == 1)
+{
+    // добавляем нечетный элемент старого массива в новый массив
+    NewArr[j] = MyArr[j];
+}
+else { }
+
+Console.WriteLine($"[{(String.Join(", ", NewArr))}]");
+
+
+// if (count % 2 != 0)
+//     NewArr[j] = MyArr[j];
+// Console.WriteLine(String.Join(" ", NewArr));
+
+//  }
+// static void Main()
+// {
+//    // MyTask32();
+
+//     // bool nInA=MyTask33();
+//     // Console.WriteLine( nInA ? "Да" : "Нет");
+
+//    // int count=MyTask35();
+
+// }
+
+
+//}
